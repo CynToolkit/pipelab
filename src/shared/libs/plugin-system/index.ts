d21172d608d0b16c0ca567af597d1b271c1b655f
Quentin Goinaud <quentin.goinaud@gmail.com>
2024-08-08 08:21:36 +0200

    add sleep

--- src/shared/libs/plugin-system/index.ts
+++ src/shared/libs/plugin-system/index.ts
@@ -1,38 +1,43 @@
-import { createNodeDefinition } from "@cyn/plugin-core";
-import { logAction, logActionRunner } from "./log.js";
-import { branchCondition, branchConditionRunner } from "./branch.js";
-import { forLoop, ForLoopRunner } from "./for.js";
-import { manualEvent, manualEvaluator } from "./manual.js";
-import { alertAction, alertActionRunner } from "./alert.js";
+import { createNodeDefinition } from '@cyn/plugin-core'
+import { logAction, logActionRunner } from './log.js'
+// import { branchCondition, branchConditionRunner } from './branch.js'
+// import { forLoop, ForLoopRunner } from './for.js'
+import { manualEvent, manualEvaluator } from './manual.js'
+import { alertAction, alertActionRunner } from './alert.js'
+import { sleepAction, sleepActionRunner } from './sleep.js'
 
 export default createNodeDefinition({
-    id: 'system',
-    description: 'System',
-    name: 'System',
-    icon: {
-        type: "icon",
-        icon: 'mdi-cog-outline'
+  id: 'system',
+  description: 'System',
+  name: 'System',
+  icon: {
+    type: 'icon',
+    icon: 'mdi-cog-outline'
+  },
+  nodes: [
+    {
+      node: logAction,
+      runner: logActionRunner
     },
-    nodes: [
-        {
-            node: logAction,
-            runner: logActionRunner,
-        },
-        // {
-        //     node: branchCondition,
-        //     runner: branchConditionRunner,
-        // },
-        // {
-        //     node: forLoop,
-        //     runner: ForLoopRunner
-        // },
-        {
-            node: manualEvent,
-            runner: manualEvaluator
-        },
-        {
-            node: alertAction,
-            runner: alertActionRunner,
-        }
-    ]
+    // {
+    //     node: branchCondition,
+    //     runner: branchConditionRunner,
+    // },
+    // {
+    //     node: forLoop,
+    //     runner: ForLoopRunner
+    // },
+    {
+      node: manualEvent,
+      runner: manualEvaluator
+    },
+    {
+      node: alertAction,
+      runner: alertActionRunner
+    },
+    {
+      node: sleepAction,
+      runner: sleepActionRunner
+    }
+  ]
 })
