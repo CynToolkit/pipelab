78e0c89c36ec13ae08c851864d0c96a17124c3d3
Quentin Goinaud <quentin.goinaud@gmail.com>
2024-08-08 07:29:46 +0200

    add steam upload description

--- src/shared/libs/plugin-steam/upload-to-steam.ts
+++ src/shared/libs/plugin-steam/upload-to-steam.ts
@@ -1,99 +1,109 @@
-import { createAction, createActionRunner } from "@cyn/plugin-core";
+import { createAction, createActionRunner } from '@cyn/plugin-core'
 
-export const FLOW_INPUT_ID = "steam-flow-input";
-export const FLOW_OUTPUT_ID = "steam-flow-output";
+export const FLOW_INPUT_ID = 'steam-flow-input'
+export const FLOW_OUTPUT_ID = 'steam-flow-output'
 
-export const ID = "steam-upload";
+export const ID = 'steam-upload'
 
 export const uploadToSteam = createAction({
   id: ID,
-  name: "Upload to Steam",
-  description: "Upload a folder to Steam",
-  icon: "",
-  displayString: "`Upload ${params.folder} to steam`",
+  name: 'Upload to Steam',
+  description: 'Upload a folder to Steam',
+  icon: '',
+  displayString: '`Upload ${params.folder} to steam`',
   meta: {},
   params: {
     sdk: {
-      value: "",
-      label: "Steam Sdk path",
+      value: '',
+      label: 'Steam Sdk path',
       control: {
-        type: "path",
+        type: 'path',
         options: {
           properties: ['openDirectory']
         }
-      },
+      }
     },
     username: {
-      value: "",
-      label: "Steam username",
+      value: '',
+      label: 'Steam username',
       control: {
-        type: "input",
+        type: 'input',
         options: {
-          kind: "text",
-        },
-      },
+          kind: 'text'
+        }
+      }
     },
     appId: {
-      value: "",
-      label: "App Id",
+      value: '',
+      label: 'App Id',
       control: {
-        type: "input",
+        type: 'input',
         options: {
-          kind: "text",
-        },
-      },
+          kind: 'text'
+        }
+      }
     },
     depotId: {
-      value: "",
-      label: "Depot Id",
+      value: '',
+      label: 'Depot Id',
       control: {
-        type: "input",
+        type: 'input',
         options: {
-          kind: "text",
-        },
-      },
+          kind: 'text'
+        }
+      }
+    },
+    description: {
+      label: 'Description',
+      value: '',
+      control: {
+        type: 'input',
+        options: {
+          kind: 'text'
+        }
+      }
     },
     folder: {
-      value: "",
-      label: "Folder to upload",
+      value: '',
+      label: 'Folder to upload',
       control: {
-        type: "path",
+        type: 'path',
         options: {
           properties: ['openDirectory']
         }
-      },
-    },
+      }
+    }
   },
-  outputs: {},
-});
+  outputs: {}
+})
 
 export const uploadToSteamRunner = createActionRunner<typeof uploadToSteam>(
   async ({ log, inputs, cwd }) => {
-    const { join, dirname } = await import("path");
-    const { platform } = await import("os");
-    const { chmod, mkdir, writeFile } = await import("fs/promises");
-    const { execa, execaCommand } = await import("execa");
+    const { join, dirname } = await import('path')
+    const { platform } = await import('os')
+    const { chmod, mkdir, writeFile } = await import('fs/promises')
+    const { execa } = await import('execa')
 
-    log("uploading to steam");
-    const { folder, appId, sdk, depotId, username } = inputs;
+    log('uploading to steam')
+    const { folder, appId, sdk, depotId, username, description } = inputs
 
-    log("folder", folder);
+    log('folder', folder)
 
-    const buildOutput = join(cwd, "steam", "output");
-    const scriptPath = join(cwd, "steam", "script.vdf");
+    const buildOutput = join(cwd, 'steam', 'output')
+    const scriptPath = join(cwd, 'steam', 'script.vdf')
 
     await mkdir(buildOutput, {
-      recursive: true,
+      recursive: true
     })
 
     await mkdir(dirname(scriptPath), {
-      recursive: true,
+      recursive: true
     })
 
     const script = `"AppBuild"
 {
 	"AppID" "${appId}" // your AppID
-	"Desc" "This is a simple build script" // internal description for this build
+	"Desc" "${description}" // internal description for this build
 
 	"ContentRoot" "${folder}" // root content folder, relative to location of this file
 	"BuildOutput" "${buildOutput}" // build output folder for build logs and build cache files
@@ -110,31 +120,31 @@ export const uploadToSteamRunner = createActionRunner<typeof uploadToSteam>(
 			}
 		}
 	}
-}`;
+}`
 
-    log("script", script);
+    log('script', script)
 
     let builderFolder = 'builder'
     if (platform() === 'linux') {
       builderFolder += '_linux'
     } else if (platform() === 'darwin') {
-      builderFolder += "_osx"
+      builderFolder += '_osx'
     }
 
-    let cmd = "steamcmd";
-    if (platform() === "linux") {
-      cmd += ".sh";
-    } else if (platform() === "darwin") {
-      cmd += ".sh";
-    } else if (platform() === "win32") {
-      cmd += ".exe";
+    let cmd = 'steamcmd'
+    if (platform() === 'linux') {
+      cmd += '.sh'
+    } else if (platform() === 'darwin') {
+      cmd += '.sh'
+    } else if (platform() === 'win32') {
+      cmd += '.exe'
     }
 
-    const steamcmdPath = join(sdk, 'tools', 'ContentBuilder', builderFolder, cmd);
+    const steamcmdPath = join(sdk, 'tools', 'ContentBuilder', builderFolder, cmd)
 
     log('steamcmdPath', steamcmdPath)
 
-    if (platform() === "linux" || platform() === "darwin") {
+    if (platform() === 'linux' || platform() === 'darwin') {
       log('Adding "execute" permissions')
       await chmod(steamcmdPath, 0o755)
     }
@@ -143,10 +153,10 @@ export const uploadToSteamRunner = createActionRunner<typeof uploadToSteam>(
     await writeFile(scriptPath, script, 'utf8')
 
     log('Executing steamcmd')
-    await execa(steamcmdPath, ["+login", username, "+run_app_build", scriptPath, '+quit'], {
+    await execa(steamcmdPath, ['+login', username, '+run_app_build', scriptPath, '+quit'], {
       stdout: 'inherit',
       stderr: 'inherit'
     })
     log('Done uploading')
   }
-);
+)
