eda90139f83c4e4b6ce110f57bf9e79615795cfb
Quentin Goinaud <quentin.goinaud@gmail.com>
2024-08-09 08:47:52 +0200

    -

--- src/shared/supabase.ts
+++ src/shared/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from './database.types'
 
 export const supabase = createClient<Database>(
-  // @ts-expect-error
+  // @ts-expect-error supabase
   import.meta.env.VITE_SUPABASE_URL,
-  // @ts-expect-error
+  // @ts-expect-error supabase
   import.meta.env.VITE_SUPABASE_ANON_KEY
 )
