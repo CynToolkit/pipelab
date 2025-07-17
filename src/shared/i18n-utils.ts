1ac944e19ba0cd24b952539e534e8c833f8682d9
Quentin Goinaud <quentin.goinaud@gmail.com>
2025-07-17 17:18:09 +0200

    add more languages to translation

--- src/shared/i18n-utils.ts
+++ src/shared/i18n-utils.ts
@@ -1,5 +1,9 @@
 import en_US from './i18n/en_US.json'
 import fr_FR from './i18n/fr_FR.json'
+import pt_BR from './i18n/pt_BR.json'
+import zh_CN from './i18n/zh_CN.json'
+import es_ES from './i18n/es_ES.json'
+import de_DE from './i18n/de_DE.json'
 
 type MessageSchema = typeof en_US
-export { MessageSchema, en_US, fr_FR }
+export { MessageSchema, en_US, fr_FR, pt_BR, zh_CN, es_ES, de_DE }
