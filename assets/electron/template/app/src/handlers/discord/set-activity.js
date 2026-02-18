2b85c2e201b2badae6440fe68e0b151d5ca07167
Quentin Goinaud <quentin.goinaud@gmail.com>
2026-02-18 17:16:32 +0100

    feat(discord): add error handling and timestamp validation for setActivity

--- assets/electron/template/app/src/handlers/discord/set-activity.js
+++ assets/electron/template/app/src/handlers/discord/set-activity.js
@@ -2,7 +2,7 @@
  * @param {import('@pipelab/core').MakeInputOutput<import('@pipelab/core').DiscordSetActivity, 'input'>} json
  * @param {import('ws').WebSocket} ws
  * @param {import('electron').BrowserWindow} mainWindow
- * @param {Object} client
+ * @param {import('discord-rpc').Client} rpc
  */
 export default async (json, ws, mainWindow, rpc) => {
   console.log('json', json)
@@ -22,26 +22,44 @@ export default async (json, ws, mainWindow, rpc) => {
     return
   }
 
-  rpc.setActivity({
-    details,
-    state,
-    startTimestamp,
-    largeImageKey,
-    largeImageText,
-    smallImageKey,
-    smallImageText,
-    instance: false
-  })
+  const startTimestampAsNumber = Number.parseInt(startTimestamp)
 
-  /**
-   * @type {import('@pipelab/core').MakeInputOutput<import('@pipelab/core').DiscordSetActivity, 'output'>}
-   */
-  const result = {
-    url: json.url,
-    correlationId: json.correlationId,
-    body: {
-      success: true
+  try {
+    await rpc.setActivity({
+      details,
+      state,
+      startTimestamp: startTimestampAsNumber,
+      largeImageKey,
+      largeImageText,
+      smallImageKey,
+      smallImageText,
+      instance: false
+    })
+
+    /**
+     * @type {import('@pipelab/core').MakeInputOutput<import('@pipelab/core').DiscordSetActivity, 'output'>}
+     */
+    const result = {
+      url: json.url,
+      correlationId: json.correlationId,
+      body: {
+        success: true
+      }
     }
+    ws.send(JSON.stringify(result))
+  } catch (e) {
+    console.error('e', e)
+    /**
+     * @type {import('@pipelab/core').MakeInputOutput<import('@pipelab/core').DiscordSetActivity, 'output'>}
+     */
+    const result = {
+      url: json.url,
+      correlationId: json.correlationId,
+      body: {
+        success: false,
+        error: e.message
+      }
+    }
+    ws.send(JSON.stringify(result))
   }
-  ws.send(JSON.stringify(result))
 }
