5f0b98c00f4c34784d7741fccc14d1ad9eafc1cf
Quentin Goinaud <quentin.goinaud@gmail.com>
2026-04-02 15:39:10 +0200

    refactor: simplify project migration versioning and add final version migration step

--- packages/shared/src/migrations/projects.ts
+++ packages/shared/src/migrations/projects.ts
@@ -1,10 +1,10 @@
-import { createMigration, createMigrator, initialVersion, SemVer } from "@pipelab/migration";
+import { createMigration, createMigrator, initialVersion, finalVersion, } from "@pipelab/migration";
 import { FileRepoV1, FileRepoV2, FileRepo } from "../config/projects";
 
 export const fileRepoMigrator = createMigrator<FileRepoV1, FileRepo>();
 
 export const defaultFileRepo = fileRepoMigrator.createDefault({
-  version: "2.0.0" as SemVer,
+  version: "2.0.0",
   projects: [
     {
       id: "main",
@@ -19,7 +19,7 @@ export const fileRepoMigrations = fileRepoMigrator.createMigrations({
   defaultValue: defaultFileRepo,
   migrations: [
     createMigration<never, FileRepoV1, FileRepoV2>({
-      version: "1.0.0" as SemVer,
+      version: "1.0.0",
       up: (state) => {
         const pipelines: FileRepoV2["pipelines"] = Object.entries(state.data || {}).map(
           ([id, file]) => {
@@ -31,7 +31,7 @@ export const fileRepoMigrations = fileRepoMigrator.createMigrations({
           },
         );
         return {
-          version: "2.0.0" as SemVer,
+          version: "2.0.0",
           projects: [
             {
               id: "main",
@@ -44,6 +44,12 @@ export const fileRepoMigrations = fileRepoMigrator.createMigrations({
       },
       down: initialVersion,
     }),
-    // ...
+    createMigration<FileRepoV1, FileRepoV2, never>({
+      version: "2.0.0",
+      up: finalVersion,
+      down: (state) => {
+        throw new Error("Cannot downgrade to version 1.0.0");
+      },
+    }),
   ],
 });
