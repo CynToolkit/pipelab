7ea043b0d1901a7f383b297f4b77075ac7bc3f11
Quentin Goinaud <quentin.goinaud@gmail.com>
2026-09-10 08:49:48 +0000

    fix(build): use TypeScript declaration resolver

--- plugins/plugin-discord/tsdown.config.ts
+++ plugins/plugin-discord/tsdown.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   dts: {
-    resolve: false,
+    resolver: "tsc",
     tsconfigRaw: {
       references: [],
     },
