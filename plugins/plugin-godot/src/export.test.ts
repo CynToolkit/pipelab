5a99c22f43439888e69a070f2607590d46ba15cf
Quentin Goinaud <quentin.goinaud@gmail.com>
2026-09-19 14:32:12 +0000

    test: normalize Godot paths across platforms

--- plugins/plugin-godot/src/export.test.ts
+++ plugins/plugin-godot/src/export.test.ts
@@ -3,9 +3,9 @@ import { godotPresetMatchesTarget, godotTemplateDirectories } from "./export";
 
 describe("Godot export support", () => {
   it("uses the platform-specific template directory", () => {
-    expect(godotTemplateDirectories("win32", "C:/Users/test")[0]).toContain("AppData");
-    expect(godotTemplateDirectories("darwin", "/Users/test")[0]).toContain("Library/Application Support/Godot");
-    expect(godotTemplateDirectories("linux", "/home/test")[0]).toBe("/home/test/.local/share/godot/export_templates");
+    expect(godotTemplateDirectories("win32", "C:/Users/test")[0].replaceAll("\\", "/")).toContain("AppData");
+    expect(godotTemplateDirectories("darwin", "/Users/test")[0].replaceAll("\\", "/")).toContain("Library/Application Support/Godot");
+    expect(godotTemplateDirectories("linux", "/home/test")[0].replaceAll("\\", "/")).toBe("/home/test/.local/share/godot/export_templates");
   });
 
   it("matches discovered preset platforms to release targets", () => {
